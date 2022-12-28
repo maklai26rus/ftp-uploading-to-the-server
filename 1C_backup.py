de8b50cffcc6f579b0ecbc7acf98d21487b15b75
Maklai <maklai26rus@mail.ru>
2022-12-28 08:11:34 +0300

    точка сохранения

--- 1C_backup.py
+++ 1C_backup.py
@@ -1,66 +1,48 @@
-import ftplib
 from ftplib import FTP
-import json
+# import json
 import os
 from datetime import date
 
-setting_server = 'ftp_server.json'
-setting_folder = "folder_save.json"
+from setting_ftp import log_txt, ftp_setting_json, folder_json
 
-
-def path_normal(path):
-    """Модюль преобразующий в правильный путь """
-    _pn = os.path.normpath(os.path.join(path))
-
-    return _pn
-
-
-def read_json_file(file):
-    """
-    Читаем json file
-    :param file:
-    :return: возращает словарь из json
-    """
-    with open(file, 'r', encoding='utf-8') as ff:
-        j = json.load(ff)
-    return j
-
-
-pn_ss = path_normal(setting_server)
-pn_sf = path_normal(setting_folder)
-
-ftp_setting_json = read_json_file(pn_ss)
-folder_json = read_json_file(pn_sf)
-
-path_file = folder_json['folder']['path']
+fs = folder_json()
+fsj = ftp_setting_json()
+path_file = fs['folder']['path']
 
 
 def run():
     """Вызгрузка файлов на сервер FTP"""
-    with FTP(host=ftp_setting_json['ftp']['host']) as ftp:
-        ftp.login(user=ftp_setting_json['ftp']['user'], passwd=ftp_setting_json['ftp']['password'])
-        if ftp_setting_json['ftp']['catalog'] is not None:
-            ftp.cwd(ftp_setting_json['ftp']['catalog'])
+    with FTP(host=fsj['ftp']['host']) as ftp:
+        ftp.login(user=fsj['ftp']['user'], passwd=fsj['ftp']['password'])
+        if fsj['ftp']['catalog'] is not None:
+            try:
+                ftp.cwd(fsj['ftp']['catalog'])
+            except:
+                ftp.mkd(fsj['ftp']['catalog'])
+                ftp.cwd(fsj['ftp']['catalog'])
 
         pf = os.listdir(path_file)
 
         try:
-            ftp.cwd(folder_json['folder']['catalog'])
+            ftp.cwd(fs['folder']['catalog'])
         except:
-            ftp.mkd(folder_json['folder']['catalog'])
-            ftp.cwd(folder_json['folder']['catalog'])
+            ftp.mkd(fs['folder']['catalog'])
+            ftp.cwd(fs['folder']['catalog'])
 
         try:
-            ftp.cwd(folder_json['folder']['new_archive_folder'] + "_" + str(date.today()))
+            ftp.cwd(fs['folder']['new_archive_folder'])
         except:
-            ftp.mkd(folder_json['folder']['new_archive_folder'] + "_" + str(date.today()))
-            ftp.cwd(folder_json['folder']['new_archive_folder'] + "_" + str(date.today()))
+            ftp.mkd(fs['folder']['new_archive_folder'])
+            ftp.cwd(fs['folder']['new_archive_folder'])
 
         for i in range(len(pf)):
 
             try:
                 with open(path_file + '\\' + pf[i], 'rb') as f:
                     ftp.storbinary('STOR ' + pf[i], f)
+
+                with open(log_txt, 'a', encoding='utf-8') as f:
+                    f.write(f"Копирование {str(pf[i])} сделано в {str(date.today())} \n")
             except:
                 pass
         # data = ftp.retrlines('LIST')
--- ksamu_backup.py
+++ ksamu_backup.py
@@ -2,28 +2,7 @@ import os
 import json
 from ftplib import FTP
 from datetime import date
-
-
-def path_normal(path):
-    """Модюль преобразующий в правильный путь """
-    _pn = os.path.normpath(os.path.join(path))
-
-    return _pn
-
-
-def read_json_file(file):
-    """
-    Читаем json file
-    :param file:
-    :return: возращает словарь из json
-    """
-    with open(file, 'r', encoding='utf-8') as ff:
-        j = json.load(ff)
-    return j
-
-
-setting_folder = "folder_save.json"
-setting_server = 'ftp_server.json'
+from setting_ftp import setting_server, log_txt, setting_folder, path_normal, read_json_file
 
 j_sf = read_json_file(setting_folder)
 ftp_setting_json = read_json_file(setting_server)
@@ -55,19 +34,23 @@ def run():
             ftp.cwd(j_sf['folder']['catalog'])
 
         try:
-            ftp.cwd(j_sf['folder']['new_archive_folder'] + "_" + str(date.today()))
+            ftp.cwd(j_sf['folder']['new_archive_folder'])
         except:
-            ftp.mkd(j_sf['folder']['new_archive_folder'] + "_" + str(date.today()))
-            ftp.cwd(j_sf['folder']['new_archive_folder'] + "_" + str(date.today()))
+            ftp.mkd(j_sf['folder']['new_archive_folder'])
+            ftp.cwd(j_sf['folder']['new_archive_folder'])
+
+        arh = 'arhiv' + '.' + split_path[-1].split('.')[-1]
 
         try:
-            # print(file_save)
             with open(file_save, 'rb') as f:
-                ftp.storbinary('STOR ' + split_path[-1], f)
+                # ftp.storbinary('STOR ' + split_path[-1], f)
+                ftp.storbinary('STOR ' + arh, f)
         except:
             print('Ошибка')
-            pass
         # data = ftp.retrlines('LIST')
+        with open(log_txt, 'a', encoding='utf-8') as f:
+            f.write(f'Копирование {str(arh)} сделано в {str(date.today())} \n')
+
         ftp.quit()
 
 
