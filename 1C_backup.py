df0c660690dfeb846c78c89d0bb50f0a607673ff
Maklai <maklai26rus@mail.ru>
2023-01-16 15:20:04 +0300

    Создает архив на диске Д Сам архив отправляет на Сервер

--- 1C_backup.py
+++ 1C_backup.py
@@ -2,16 +2,25 @@ from ftplib import FTP
 # import json
 import os
 from datetime import date
+import shutil
 
-from setting_ftp import log_txt, ftp_setting_json, folder_json
+from setting_ftp import log_txt, ftp_setting_json, folder_json, path_normal
 
 fs = folder_json()
 fsj = ftp_setting_json()
 path_file = fs['folder']['path']
+path_end = path_normal(fs['folder']['path_end'])
 
 
 def run():
-    """Вызгрузка файлов на сервер FTP"""
+    """Вызгрузка файлов на сервер FTP
+    Сначала открывает фтп сервер. потом проверяет есть ли там папки
+    Да/нет Созадть /Нет
+    Архивирует файл на сервере в указаную папку
+    path_end
+
+
+    """
     with FTP(host=fsj['ftp']['host']) as ftp:
         ftp.login(user=fsj['ftp']['user'], passwd=fsj['ftp']['password'])
         if fsj['ftp']['catalog'] is not None:
@@ -21,7 +30,8 @@ def run():
                 ftp.mkd(fsj['ftp']['catalog'])
                 ftp.cwd(fsj['ftp']['catalog'])
 
-        pf = os.listdir(path_file)
+        # pf = os.listdir(path_file)
+        pp = path_normal(path_end + '\\' + 'Arhiv')
 
         try:
             ftp.cwd(fs['folder']['catalog'])
@@ -35,16 +45,26 @@ def run():
             ftp.mkd(fs['folder']['new_archive_folder'])
             ftp.cwd(fs['folder']['new_archive_folder'])
 
-        for i in range(len(pf)):
+        shutil.make_archive(pp, 'zip', path_file)
+        pe = os.listdir(path_end)
 
-            try:
-                with open(path_file + '\\' + pf[i], 'rb') as f:
-                    ftp.storbinary('STOR ' + pf[i], f)
+        with open(path_end + '\\' + pe[0], 'rb') as f:
+            ftp.storbinary('STOR ' + pe[0], f)
 
-                with open(log_txt, 'a', encoding='utf-8') as f:
-                    f.write(f"Копирование {str(pf[i])} сделано в {str(date.today())} \n")
-            except:
-                pass
+        with open(log_txt, 'a', encoding='utf-8') as f:
+            f.write(f"Копирование {str(pp)} сделано в {str(date.today())} \n")
+
+        # Нужна если цель копировать все файлы без архива
+        # for i in range(len(pf)):
+        #
+        #     try:
+        #         with open(path_file + '\\' + pf[i], 'rb') as f:
+        #             ftp.storbinary('STOR ' + pf[i], f)
+        #
+        #         with open(log_txt, 'a', encoding='utf-8') as f:
+        #             f.write(f"Копирование {str(pf[i])} сделано в {str(date.today())} \n")
+        #     except:
+        #         pass
         # data = ftp.retrlines('LIST')
         ftp.quit()
 
